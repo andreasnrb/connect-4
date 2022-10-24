f963eb60ba855e0160b5a6e7989004d791598fb3
Andreas Nurbo <andreasnrb@users.noreply.github.com>
2022-10-24 22:27:36 +0200

    Add local game storage

---
+++ src/Logic/Storage.ts
@@ -0,0 +1,22 @@
+
+class GameStorage {
+    addPlacement(placement:[number,number]) {
+        let history = this.getHistory();
+        history.push(placement);
+        this.setHistory(history);
+    }
+    removeLatestPlacement() {
+        let history = this.getHistory();
+        history.pop();
+        this.setHistory(history);
+    }
+    setHistory(history:[]) {
+        localStorage.setItem('history', JSON.stringify(history));
+    }
+    getHistory() {
+        let historyString = localStorage.getItem('history') ?? JSON.stringify([]);
+        return JSON.parse(historyString);
+    }
+}
+
+export default GameStorage;
